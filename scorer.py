0cc7f3621977dd11585bae8df88ac000cbe79697
shipitandrei <andreitherickroller@gmail.com>
2026-08-19 19:42:19 +1000

    Remove redundant “player”
    Didn’t I already remove this

--- scorer.py
+++ scorer.py
@@ -60,7 +60,7 @@ def t_tennis(e_score: int):
             #Decides who won based on who has the higher score.
             #The person with more points is usually the winner.
 
-            print(winner, "Player wins")
+            print(winner, "wins")
             #Finally tell the winner that they have won the game.
             #Congratulations you hit the ball better than the other guy.
 
